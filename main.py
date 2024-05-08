68145f3682c643d9688c38b1fb49611d2ed6684e
Олег Карапоткин <karapotkin-oleg@yandex.ru>
2024-05-08 19:14:27 +0300

    Небольшие измения в запуске ботов

--- main.py
+++ main.py
@@ -3,15 +3,11 @@ from bot_parse import bot_parse
 from core.init_db import create_super_user
 
 
-
 def main():
     bot_parse.run()
 
 
 loop = asyncio.get_event_loop()
-tasks = [
-    loop.create_task(create_super_user()),
-    loop.create_task(main())
-]
-loop.run_until_complete(asyncio.wait(tasks))
+loop.run_until_complete(create_super_user())
+loop.run_forever(main())
 loop.close()
